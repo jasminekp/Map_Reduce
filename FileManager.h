a367032bb35f85bf9cd7a741e1a4dc0e3d4d719e
Jasmine Parmar <jasmine.k.parmar@gmail.com>
2023-04-22 18:09:21 -0400

    Map class draft
    Commit following files:
    FIleManager.h (test header)
    Map.cpp
    Map_Reduce.cpp (generated by VS)
    Test1.cpp
    & general visual studio files

---
+++ FileManager.h
@@ -0,0 +1,13 @@
+
+//sample setup for FileManager header file
+
+using namespace std;
+
+class FileManager
+{
+public:
+	void writeIntermediateFile(string fileName, string distinctWord, int value);
+	string readFile();
+
+
+};
---
+++ Map_Reduce.cpp
@@ -0,0 +1,14 @@
+// Map_Reduce.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// This file is meant to act as the Executive class
+//
+
+#include <iostream>
+#include "Map.cpp"
+
+
+int test()
+{
+	//will need to call the executive class here to start the project
+    std::cout << "Hello World!\n";
+	return 0;
+}
