373675b4c765d0663b45bae0e30b27a4def67e34
Jasmine Parmar <jasmine.k.parmar@gmail.com>
2023-04-24 12:51:20 -0400

    Update Map files
    1) Removed FileManager.h and Map_Reduce.cpp since intent was testing.
    2) Map.cpp also modified with correct reference to FileManager methods
    3) Added Map.h for modularity

--- Map.cpp
+++ Map.cpp
@@ -1,85 +1,126 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <list>
-#include <utility>
-#include <sstream>
-#include <cctype>
-#include "FileManager.h"
-using namespace std;
+#include "Map.h"
 
-class Map
+/*
+ * The Map class takes the raw data line from the input file and tokenizes the 
+ * raw data line into distinct words. This class then buffers the distinct words into an
+ * intermediate file.
+ *
+ */
+
+
+/**
+ * Checks if the buffer size is greater than the threshold and writes to the intermediate file
+ */
+bool Map::Export(std::string distinctWord, int value, std::string fileName, bool eof)
 {
 
-private:
-	FileManager fm;
-	int Threshold = 100;
-	list<pair<string, int>> buffer;
-	vector<string> distinctWords;
-
-	bool TokenizeValue(string rawDataLine)
+	if (eof)
 	{
-		// split string by word and count the number of times it appears in line
-		if (rawDataLine.empty() || rawDataLine.length() <= 0)
-		{
-			return false;
-		}
+		std::string stringBufferData;
+		stringBufferData += distinctWord + ":" + std::to_string(value);
 
-		distinctWords.clear();
-		string temp;
-		istringstream stream(rawDataLine);
+		fm->write(intermediateDirectory + fileName, stringBufferData, std::ios::app);
 
-		// read the stream of raw data into temp and split by white space
-		while (getline(stream, temp, ' '))
+		if (!buffer.empty())
 		{
-			// convert to lowercase and remove punctuation
-			for (int i = 0; i < temp.size(); i++)
+			std::string stringBufferData;
+			for (auto data : buffer)
 			{
-				temp[i] = tolower(temp[i]);
-
-				if (ispunct(temp[i]))
-				{
-					temp.erase(i--, 1);
-				}
+				stringBufferData += data.first + ":" + std::to_string(data.second) + "\n";
 			}
 
-			distinctWords.push_back(temp);
-		}
-
-		return true;
-	}
-
-public:
-	bool Export(string distinctWord, int value)
-	{
-		if (buffer.size() > Threshold)
-		{
-			// writeIntermediateFile needed for the fm class
-			string fileName;
-			fm.writeIntermediateFile(fileName, distinctWord, value);
-		}
-		else
-		{
-			buffer.push_back(make_pair(distinctWord, 1));
+			fm->write(intermediateDirectory + fileName, stringBufferData, std::ios::app);
+			buffer.clear();
 		}
 	}
-
-	bool map(string fileName, std::string value)
+	else if (buffer.size() >= Threshold)
 	{
-		// read in the string from the file through the FileManager class
-		string rawData = fm.readFile();
+		std::string stringBufferData;
+		std::string newLineChar = "\n";
 
-		// tokenize the string into distinct words and remove punctuation, strings, etc
-		if (TokenizeValue(value))
+
+		for (auto data : buffer)
 		{
-			// loop through the distinctWords and add to buffer
-			for (auto word : distinctWords)
+			stringBufferData += data.first + ":" + std::to_string(data.second) + newLineChar;
+
+		}
+		
+		fm->write(intermediateDirectory + fileName, stringBufferData, std::ios::app);
+		buffer.clear();
+	}
+	else
+	{
+		buffer.push_back(make_pair(distinctWord, 1));
+	}
+
+	return 0;
+}
+
+
+/**
+ * Takes in the file name and the raw data string
+ */
+bool Map::map(std::string fileName, std::string value, bool eof)
+{
+	// tokenize the string into distinct words and remove punctuation, strings, etc
+	std::vector<std::string> vec = TokenizeValue(value);
+
+	for (auto word : vec)
+	{
+		// export the distinct word and the value
+		Export(word, 1, fileName, eof);
+	}
+
+	return 0;
+}
+
+
+/**
+ * Map Constructor
+ */
+Map::Map(FileManager *fm, std::string intermediateDir)
+{
+	fm = fm;
+	this->intermediateDirectory = intermediateDir;
+
+}
+
+
+/**
+ * TokenizeValue splits the raw data string from the text file into distinct words and separates by white space
+ */
+std::vector<std::string> Map::TokenizeValue(std::string rawDataLine)
+{
+	std::vector<std::string> distinctWords{};
+	std::string temp;
+
+	//replace all the new line characters
+	std::replace(rawDataLine.begin(), rawDataLine.end(), '\n', ' ');
+
+	std::istringstream stream(rawDataLine);
+
+
+	// read the stream of raw data into temp and split by white space
+	while (getline(stream, temp, ' '))
+	{
+		if (temp.empty())
+			continue;
+
+		for (int i = 0; i < temp.size(); i++)
+		{
+			// convert to lowercase
+			temp[i] = tolower(temp[i]);
+
+			//remove punctuation + special characters
+			if (ispunct(temp[i]) || (temp[i] == ':') || (temp[i] == '-') || (temp[i] == '_') || (temp[i] >= '0' && temp[i] <= '9'))
 			{
-				// export the distinct word and the value
-				Export(word, 1);
+				temp.erase(i--, 1);
 			}
-
-			return 0;
 		}
+
+		distinctWords.push_back(temp);
+
 	}
-};
+
+	return distinctWords;
+}
