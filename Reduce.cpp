4894a71890fa4ad37f01ba74461877bd0d9345c1
Humberto Dominguez Jr <128658763+betito44@users.noreply.github.com>
2023-04-25 22:45:29 -0500

    Updated Reduce Files
    Updated Reduce.cpp

--- Reduce.cpp
+++ Reduce.cpp
@@ -1,75 +1,57 @@
-/*
-
-Humberto Dominguez
-Professor Scott Roueche
-CSE 687- Object Oriented Design
-Syracuse University
-Phase 1 - Reduce Header
-4/20/2023
-
-This class was created in part of creating the MapReduce code for Phase 1 for
-CSE 687. The Reduce class's primary purpose is to consolidate all of the data that has been gather thus far.
-
-Since this is the cpp portion of the class, this will define how and where the the member and methods will be
-used.
-*/
-
-//libraries that will be used in this class
 #include "Reduce.h"
 
-
-
-//Constructor. Creation of the class
-Reduce::Reduce() {
-
-}
-
-//Constructor with one arguement. Creation of the class
-Reduce::Reduce(string outputDirectoryLocation) : outputDir{ outputDirectoryLocation }
-{}
-
-//Constructor. Creation of the class
-Reduce::~Reduce() {
-
+//accept outputdir as parameter
+Reduce::Reduce(std::string outputDir)
+{
+	this->outputDir = outputDir;
 }
 
 
 //reduce method. Iterators through the vector and sums up all of the occurences
-bool Reduce::reduce(string word, std::vector<int> occurences) {
+bool Reduce::reduce(std::string word, std::vector<int> occurences) {
 
-    int totalOccurences = 0; // variable to store the total amount of time a word was used
+	int totalOccurences = 0; // variable to store the total amount of time a word was used
 
-    //for loop used to iterate through each element of the vector
-    for (auto& timesUsed : occurences) {
-        totalOccurences += timesUsed; //each time through the loop, we are recording how many times the word was said
-    }
+	//for loop used to iterate through each element of the vector
+	for (auto& timesUsed : occurences) {
+		totalOccurences += timesUsed; //each time through the loop, we are recording how many times the word was said
+	}
 
-    //exporting the data to the file 
-    string consolidatedKeyAndValue = outputDir + word + ".txt"; //creates a txt file with the 
-    fm.open(consolidatedKeyAndValue); // creates a txt file with the name of the key
-    string stringRep = std::to_string(totalOccurences); //converts the total number of Occurences to a string
-    string keyPlusOccurences = word + " " + stringRep; // creates a string of the key and total occurences that will be written to the file
-    fm.write(consolidatedKeyAndValue, keyPlusOccurences); // writes the data to the correct file
-    fm.close(consolidatedKeyAndValue); //closes the correct file
-
-    //creation of the success file
-    string successFile = outputDir + word + "_SUCCESS.txt"; // creates the success file
-    fm.open(successFile); //opens the file
-    fm.write(successFile, ""); //writing to the empty file
-    fm.close(successFile); //closes the file
-
-    //WHAT IS THE FORMAT OF THE OUTPUT DIRECTORY FILE
-
-
-    return true;
+	Export(word, totalOccurences);
+	return true;
 }
 
 
-void Reduce::setoutputDir() {
-    cout << "Please type in the Directory that you would like the files to be save in.: "; //asking the user which directory they would like their file to be saved in
-    cin >> outputDir;
+
+bool Reduce::Export(std::string key, int reducedValue)
+{
+	std::string stringRep = std::to_string(reducedValue);
+	std::string keyPlusOccurences = key + " " + stringRep;
+
+	fileWriter.write(keyPlusOccurences);
+
+	return 0;
 }
 
-string Reduce::getoutputDir() {
-    return outputDir;
+
+void Reduce::start()
+{
+	fileWriter.deleteFile(this->outputDir + "SUCCESS.txt");
+
+	fileWriter.clear(this->outputDir + "out.txt");
+	fileWriter.open(this->outputDir + "out.txt", std::ios::out | std::ios::app);
+
+
 }
+
+
+void Reduce::end()
+{
+	fileWriter.close();	//closes out.txt
+
+	fileWriter.open(this->outputDir + "SUCCESS.txt", std::ios::out | std::ios::app);
+
+	fileWriter.close();	//close success.txt
+}
+
+
--- Reduce.h
+++ Reduce.h
@@ -1,62 +1,27 @@
-/*
-
-Humberto Dominguez
-Professor Scott Roueche
-CSE 687- Object Oriented Design
-Syracuse University
-Phase 1 - Reduce Header
-4/20/2023
-
-This class was created in part of creating the MapReduce code for Phase 1 for
-CSE 687. The Reduce class's primary purpose is to consolidate all of the data that has been gather thus far.
-
-Since this is the header portion of the class, this will declar all methods and members that the .cpp file
-will use.
-*/
-
-#pragma once
-
-//libraries that will be used in this class
 #include <iostream>
-#include "Reduce.h"
 #include <vector>
 #include <fstream>
-#include "Filemanager.h"
 #include <string>
+#include "FileManager.h"
 
-
-
-//Declaring which member from a specific namespace is going to be used
-using std::cout;
-using std::cin;
-using std::string;
-using std::ofstream;
-
-
+#pragma once
 
 //This will declare what members and methods will be used by the class
 class Reduce {
 
-	//declaration of public members and methods that will be seen by the user
 public:
 
-	Reduce(); // Default constructor of the class
+	Reduce(std::string outputDir); // Default constructor of the class
 
-	Reduce(string outputDirectoryLocation); //secondary constructor in which the user supplies the output dir
+	bool reduce(std::string word, std::vector<int> occurences); //reduce method that takes a string and a vector of integers as an arguement
+	bool Export(std::string key, int reducedValue);
+	void start();
+	void end();
 
-	~Reduce(); //destructor
 
-	bool reduce(string word, std::vector<int> occurences); //reduce method that takes a string and a vector of integers as an arguement
-
-	void setoutputDir(); //allows us to change the value of the outputDir
-
-	string getoutputDir(); //allows us to set the value of outputDir
-
-	//declaration of private members and methods that will not be seen by the user
 private:
-
-	string outputDir = ""; //Output Directory which will be given a value when the class is created
-
-	FileManager fm;
+	FileManager fileWriter;
+	std::string outputDir = "";
 
 };
+
